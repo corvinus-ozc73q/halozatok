bd1c69e4741acf7ba4cc7c968e0ab0b7e7afa0e2
Gábor Ferenc Korbács <gabor.korbacs@stud.uni-corvinus.hu>
2021-02-12 12:58:17 +0100

    Statikus fájlok kiszolgálása

--- Hajosteszt/Hajosteszt/Startup.cs
+++ Hajosteszt/Hajosteszt/Startup.cs
@@ -16,6 +16,7 @@ namespace Hajosteszt
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,16 +26,20 @@ namespace Hajosteszt
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseHttpsRedirection();
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
 
             app.UseRouting();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-            });
+            //app.UseEndpoints(endpoints =>
+           // {
+             //   endpoints.MapGet("/", async context =>
+               // {
+                 //   await context.Response.WriteAsync("Hello World!");
+               // });
+            //});
         }
     }
 }
