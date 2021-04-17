057f64a05261d17b8e132c957b4ede10ed3414c9
Gábor Ferenc Korbács <gabor.korbacs@stud.uni-corvinus.hu>
2021-04-17 10:35:17 +0200

    Route

--- Hajosteszt/Hajosteszt/Startup.cs
+++ Hajosteszt/Hajosteszt/Startup.cs
@@ -12,20 +12,18 @@ namespace Hajosteszt
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            
+            services.AddControllers();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+
             app.UseHttpsRedirection();
 
             app.UseDefaultFiles();
@@ -33,13 +31,14 @@ namespace Hajosteszt
 
             app.UseRouting();
 
-            //app.UseEndpoints(endpoints =>
-           // {
-             //   endpoints.MapGet("/", async context =>
-               // {
-                 //   await context.Response.WriteAsync("Hello World!");
-               // });
-            //});
+
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
         }
     }
 }
+
+    
