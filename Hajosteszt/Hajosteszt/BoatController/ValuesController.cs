018bc29787db6bc8eef8474b7823ffae50b3c37e
Gábor Ferenc Korbács <gabor.korbacs@stud.uni-corvinus.hu>
2021-04-17 11:24:12 +0200

    gitignore

---
+++ Hajosteszt/Hajosteszt/BoatController/ValuesController.cs
@@ -0,0 +1,31 @@
+﻿using Hajosteszt.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hajosteszt.BoatController
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+    }
+    
+
+[ApiController]
+    public class BoatController : ControllerBase
+    {
+        [HttpGet]
+        [Route("questions/all")]
+        public ActionResult M1()
+        {
+            HajostesztContext context = new HajostesztContext();
+            var kérdések = from x in context.Questions select x.QuestionText;
+
+            return new JsonResult(kérdések);
+        }
+    }
+}
--- Hajosteszt/Hajosteszt/Controllers/TesztController.cs
+++ Hajosteszt/Hajosteszt/Controllers/TesztController.cs
@@ -9,7 +9,21 @@ namespace Hajosteszt.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    
     public class TesztController : ControllerBase
     {
+        [HttpGet]
+        [Route("corvinus/szerverido")]
+        public IActionResult M1()
+        {
+            string pontosIdő = DateTime.Now.ToShortTimeString();
+
+            return new ContentResult
+            {
+                ContentType = System.Net.Mime.MediaTypeNames.Text.Plain, //"text/plain"
+                Content = pontosIdő
+            };
+        }
     }
 }
+
